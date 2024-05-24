0cb53db73ae50dc6486f0ac74f10e8f23066d395
Vladimir Nikitin <jo.kent@mail.ru>
2024-05-24 16:42:49 +0300

    packages, readme

--- backend/config/app.php
+++ backend/config/app.php
@@ -168,6 +168,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        NikitinUser\UserManagementModule\Lib\Providers\UserManagementModuleProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
     ])->toArray(),
 
     /*
