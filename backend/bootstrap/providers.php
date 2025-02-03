e749aa3ff744e2ff3de893ea347342dcdfe9d047
nikitinuser <jo.kent@mail.ru>
2025-02-03 06:31:27 +0500

    remove useless

--- backend/bootstrap/providers.php
+++ backend/bootstrap/providers.php
@@ -5,7 +5,5 @@ return [
     App\Providers\AuthServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
-    // UserManagementModule\Providers\UserManagementModuleProvider::class,
     Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
-    // Nikitinuser\LaravelMakeAllExtended\Providers\LaravelMakeAllExtendedProvider::class,
 ];
